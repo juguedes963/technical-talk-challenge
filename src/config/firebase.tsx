0596e0c55ea50c849b34125a5b8fe4000a4716d4
juliocesar <julio.silva@itajai.sc.gov.br>
2024-10-31 15:25:03 -0300

    initial commit

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { ENV } from "../environment";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCj1NPzPLt4pz7qwGgzGUEiw3HNjPAiXtM",
-    authDomain: "tallk-79ee7.firebaseapp.com",
-    projectId: "tallk-79ee7",
-    storageBucket: "tallk-79ee7.appspot.com",
-    messagingSenderId: "177407278803",
-    appId: "1:177407278803:web:cfafef29a618fcf77d9e26",
-    measurementId: "G-MRMW229KWH"
+    apiKey: ENV.apiKey,
+    authDomain: ENV.authDomain,
+    projectId: ENV.projectId,
+    storageBucket: ENV.storageBucket,
+    messagingSenderId: ENV.messagingSenderId,
+    appId: ENV.appId,
+    measurementId: ENV.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
---
+++ src/environment/index.ts
@@ -0,0 +1,9 @@
+export const ENV = {
+    apiKey: "AIzaSyCj1NPzPLt4pz7qwGgzGUEiw3HNjPAiXtM",
+    authDomain: "tallk-79ee7.firebaseapp.com",
+    projectId: "tallk-79ee7",
+    storageBucket: "tallk-79ee7.appspot.com",
+    messagingSenderId: "177407278803",
+    appId: "1:177407278803:web:cfafef29a618fcf77d9e26",
+    measurementId: "G-MRMW229KWH"
+};
